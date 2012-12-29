8f4607a0226792dfb89f1616faffc3d16edb3f71
Marcus Møller <marcus@powrtoch.org>
2012-12-29 17:34:41 +0100

    Added examples

---
+++ examples/list_activities.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<?php
+    // initialize lectio sdk
+    require_once("lectio.php");
+    $lectio = new lectio("402", "4763366305");
+?>
+
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Lectio PHP SDK Example: List activities of day</title>
+</head>
+<body>
+    <h2>List of activities on 29th of january, 2013 (29-01-2013)</h2>
+    
+    <?php
+        // parse the activities of given day, 29-01-2013
+        $activities = $lectio->getActivities('29-01-2013');
+        
+        $amountOfModules = sizeof($activities);
+        
+        // echo module informations
+        echo "Amount of modules: ".$amountOfModules;
+        echo $activities[0]['time'];
+        
+        for ($i = 0; $i < $amountOfModules; $i++){
+            if ($i == 0){
+                echo "<dl>";
+            }
+            
+            $htmlStr = <<<HTML__
+<dt><b>{$activities[$i]['time']}</b></dt>
+    <dd>State: {$activities[$i]['state']}</dd>
+    <dd>Class: {$activities[$i]['class']}</dd>
+    <dd>Homework: {$activities[$i]['homework']}</dd>
+        
+<br>
+HTML__;
+            echo $htmlStr;
+        }
+        echo "</dl>";
+    ?>
+</body>
+</html>
